45df085cf240b2d5aec65c0d3a72952b0dc0b76b
MentalStrang <mohamed.ramadan2393@gmail.com>
2023-08-01 19:45:48 +0300

    big change

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+function createDiv() {
+  let labelContent = prompt("Add your New Task");
+
+  if (labelContent === null || labelContent.trim() === "") {
+    return;
+  }
+
+  let parentDiv = document.createElement("div");
+  parentDiv.className = "new-task";
+
+  let newCheckBox = document.createElement("input");
+  newCheckBox.setAttribute("type", "checkbox");
+  newCheckBox.setAttribute("id", "checkbox");
+  newCheckBox.className = "newTask";
+
+  let newLabel = document.createElement("label");
+  newLabel.textContent = labelContent;
+  newLabel.setAttribute("for", "checkbox");
+  newLabel.className = "newLabel";
+
+  parentDiv.appendChild(document.createElement("br"));
+  document.querySelector(".tasks-to-do").appendChild(parentDiv);
+  parentDiv.appendChild(newCheckBox);
+  parentDiv.appendChild(newLabel);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.onclick = function () {
+    document.querySelector(".tasks-to-do").removeChild(parentDiv);
+  };
+  parentDiv.appendChild(deleteButton);
+
+  // ---------------------------------------------------------
+  newCheckBox.addEventListener("change", function () {
+    if (newCheckBox.checked) {
+      document.querySelector(".finished-tasks").appendChild(parentDiv);
+    }
+  });
+}
+
+// console.log(document.querySelector(".tasks-to-do").children);
